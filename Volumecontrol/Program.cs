223782dc3b1cac478eb04f3321e34de5b75836e0
Tassadar22 <seamas2@gmail.com>
2018-06-28 18:41:16 +0100

    first commit

---
+++ Volumecontrol/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace Volumecontrol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+            string[] arg = Environment.GetCommandLineArgs();
+            string volumeInstructions = string.Empty;
+            double currentVolume = defaultPlaybackDevice.Volume;
+            if (arg.Count()>1)
+            {
+                volumeInstructions = arg[1];
+            }
+            switch(volumeInstructions)
+            {
+                case "incBy2":
+                    currentVolume += 2;
+                    defaultPlaybackDevice.Volume = currentVolume;
+                    break;
+                case "decBy2":
+                    currentVolume -= 2;
+                    defaultPlaybackDevice.Volume = currentVolume;
+                    break;
+            }
+        }
+    }
+}
